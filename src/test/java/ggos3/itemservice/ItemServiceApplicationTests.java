5c072f312233dfd54c39c693f54f44a4dcfdc34a
ggos3 <janpll0421@gmail.com>
2023-03-19 00:28:52 +0900

    CHORE: Spring Init

---
+++ src/test/java/ggos3/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ggos3.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
