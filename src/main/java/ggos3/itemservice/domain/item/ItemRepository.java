e4b7d470b4b259a2a1de6eec1d23d05ab438933d
ggos3 <janpll0421@gmail.com>
2023-03-19 20:31:47 +0900

    FEAT: ItemRepository 추가

---
+++ src/main/java/ggos3/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,44 @@
+package ggos3.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ItemRepository {
+
+    //HashMap, long 에는 동시성 문제가 있다
+    private static final Map<Long, Item> store = new ConcurrentHashMap<>();
+    private static AtomicLong sequence = new  AtomicLong(0);
+
+    public Item save(Item item) {
+        // sequence 1 증가
+        item.setId(sequence.incrementAndGet());
+        store.put(item.getId(), item);
+
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
